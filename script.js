e554d3c3ea237de122c006b1beb6479e0ca7d604
Skylis123 <calinioana95@gmail.com>
2022-11-15 11:48:50 +0200

    Update Guess_The_Button

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 
-let nr_of_buttons,winnerButtonPosition,button;
+let numberOfButtons,winnerButtonPosition,button;
 const submitButton = document.getElementById("submitButton");
 
 function createButtons(){
-    nr_of_buttons = document.getElementById("textField").value;
-    winnerButtonPosition = Math.floor(Math.random() * nr_of_buttons) + 1;
-    for (let i = 1; i <= nr_of_buttons; ++i) {
+    numberOfButtons = document.getElementById("textField").value;
+    winnerButtonPosition = Math.floor(Math.random() * numberOfButtons) + 1;
+    for (let i = 1; i <= numberOfButtons; ++i) {
         button = document.createElement("Button");
         Object.assign(button, {
             id : i,
