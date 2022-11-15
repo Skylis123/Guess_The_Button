775a8efae8be65abb926086a8f13820e22e41dd2
Skylis123 <calinioana95@gmail.com>
2022-11-15 11:45:52 +0200

    Update Dictionary

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
 
-let nr_of_buttons,rngValue,button;
+let nr_of_buttons,winnerButtonPosition,button;
 const submitButton = document.getElementById("submitButton");
 
 function createButtons(){
     nr_of_buttons = document.getElementById("textField").value;
-    rngValue = Math.floor(Math.random() * nr_of_buttons) + 1;
+    winnerButtonPosition = Math.floor(Math.random() * nr_of_buttons) + 1;
     for (let i = 1; i <= nr_of_buttons; ++i) {
         button = document.createElement("Button");
         Object.assign(button, {
             id : i,
             innerHTML : '<img src = "normal.png">',
-            onclick : () => correctButton(rngValue,i),
+            onclick : () => correctButton(winnerButtonPosition,i),
         })
         document.getElementById("buttons_on_screen").appendChild(button);
     }
 }
 
-function correctButton (rngValue,clicked_button) {
-    if (rngValue == clicked_button) {
+function correctButton (winnerButtonPosition, clicked_button) {
+    if (winnerButtonPosition == clicked_button) {
         document.getElementById(clicked_button).innerHTML = '<img src="happy.png">'; 
     } else {
         document.getElementById(clicked_button).innerHTML = '<img src="sad.png">';
